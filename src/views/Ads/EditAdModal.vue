0a2b9bad821aaa5625c223ae20bab055a62bc027
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 04:28:09 +0300

    17.4.  Логика редактирования

--- src/views/Ads/EditAdModal.vue
+++ src/views/Ads/EditAdModal.vue
@@ -15,7 +15,9 @@
 			<v-row justify="center">
 				<v-col cols="12">
 					<v-card-text>
-						Lorem ipsum.
+						<v-text-field name="title" label="Title" type="text" v-model="editedTitle">
+						</v-text-field>
+						<v-textarea name="desc" label="Description" type="text" v-model="editedDesc" class="mb-3"></v-textarea>
 					</v-card-text>
 				</v-col>
 			</v-row>
@@ -24,8 +26,8 @@
 				<v-col cols="12">
 					<v-card-actions>
 						<v-spacer></v-spacer>
-						<v-btn>Cancel</v-btn>
-						<v-btn color="success">Save</v-btn>
+						<v-btn @click="onCancel">Cancel</v-btn>
+						<v-btn color="success" @click="onSave">Save</v-btn>
 					</v-card-actions>
 				</v-col>
 			</v-row>
@@ -38,7 +40,26 @@ export default {
 	props: ['ad'],
 	data() {
 		return {
-			modal: false
+			modal: false,
+			editedTitle: "",
+			editedDesc: "",
+		}
+	},
+	methods: {
+		onCancel() {
+			this.editedTitle = this.ad.title
+			this.editedDesc = this.ad.desc
+			this.modal = false
+		},
+		onSave() {
+			if (this.editedTitle !== '' && this.editedDesc !== '') {
+				this.$store.dispatch('updateAd', {
+					title: this.editedTitle,
+					desc: this.editedDesc,
+					id: this.ad.id
+				})
+				this.modal = false
+			}
 		}
 	}
 }
