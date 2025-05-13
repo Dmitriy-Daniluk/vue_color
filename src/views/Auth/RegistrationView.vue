e009b36dacf534fba03bb972a49b7c4f1d5255de
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 03:54:50 +0300

    13.4.  Подключение лоудера на экране регистрации

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -3,8 +3,8 @@
 		<v-row justify="center">
 			<v-col cols="12" sm="8" lg="6">
 				<v-card class="elevetion-12">
-					<v-toolbar dark color="primary" class="pl-4">
-						Registration
+					<v-toolbar dark color="primary">
+						<v-toolbar-title>Registration</v-toolbar-title>
 					</v-toolbar>
 					<v-card-text>
 						<v-form v-model="valid" ref="form" lazy-validation>
@@ -12,15 +12,16 @@
 								:rules="emailRules">
 							</v-text-field>
 							<v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password"
-								:rules="passwordRules">
-							</v-text-field>
-							<v-text-field prepend-icon="mdi-lock" name="confirm-password" label="Confirm Password" type="password" v-model="confirmPassword" :rules="confirmPasswordRules">
-							</v-text-field>
+								:rules="passwordRules"></v-text-field>
+							<v-text-field prepend-icon="mdi-lock" name="confirm-password" label="Confirm Password" type="password"
+								v-model="confirmPassword" :rules="confirmPasswordRules"></v-text-field>
 						</v-form>
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer></v-spacer>
-						<v-btn color="primary" @click="onSubmit" :disabled="!valid">Create Account</v-btn>
+						<v-btn color="primary" @click="onSubmit" :loading="loading" :disabled="!valid || loading">
+							Create Account
+						</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-col>
@@ -50,9 +51,14 @@ export default {
 			]
 		}
 	},
+	computed: {
+		loading() {
+			return this.$store.getters.loading
+		}
+	},
 	methods: {
-		onSubmit(){
-			if (this.$refs.form.validate()){
+		onSubmit() {
+			if (this.$refs.form.validate()) {
 				const user = {
 					email: this.email,
 					password: this.password
@@ -61,5 +67,5 @@ export default {
 			}
 		}
 	}
-}
+} 
 </script>
