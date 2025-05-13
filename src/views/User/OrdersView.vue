0d7e572548fccede141d1c79bd5be17ace8bd120
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 04:58:46 +0300

    19.2.  Подключение заказов на экране заказов.

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -7,8 +7,7 @@
 					<v-list-item v-for="order in orders" :key="order.id">
 						<template v-slot:prepend>
 							<v-list-item-action>
-								<v-checkbox :input-value="order.done" color="primary"
-									@click="markDone(order)"></v-checkbox>
+								<v-checkbox :input-value="order.done" color="primary" @click="markDone(order)"></v-checkbox>
 							</v-list-item-action>
 						</template>
 						<v-list-item-title>{{ order.name }}</v-list-item-title>
@@ -27,16 +26,11 @@
 <script>
 export default {
 	data() {
-		return {
-			orders: [
-				{
-					id: "123",
-					name: "Kostya",
-					phone: "+7(978)000-00-05",
-					adId: "1",
-					done: true
-				}
-			]
+		return {}
+	},
+	computed: {
+		orders() {
+			return this.$store.getters.orders
 		}
 	},
 	methods: {
