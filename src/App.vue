67e3d410e24ff392fa73b87308cd86507127b017
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 04:12:41 +0300

    15.3.  Logout

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,13 @@
 				</v-list-item>
 			</v-list>
 
+			<v-list-item @click="onLogout" v-if="isUserLoggedIn">
+				<template v-slot:prepend>
+					<v-icon icon="mdi-exit-to-app"></v-icon>
+				</template>
+				<v-list-item-title>Logout</v-list-item-title>
+			</v-list-item>
+
 		</v-navigation-drawer>
 		<v-app-bar app dark color="primary">
 			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -36,6 +43,10 @@
 				<v-btn text v-for="link in links" :key="link.title" :to="link.url">
 					<v-icon start :icon="link.icon"></v-icon>
 					{{ link.title }}
+				</v-btn>
+				<v-btn @click="onLogout" v-if="isUserLoggedIn">
+					<v-icon start icon="mdi-exit-to-app"></v-icon>
+					Logout
 				</v-btn>
 			</v-toolbar-items>
 		</v-app-bar>
@@ -93,6 +104,10 @@ export default {
 	methods: {
 		closeError() {
 			this.$store.dispatch('clearError')
+		},
+		onLogout() {
+			this.$store.dispatch('logoutUser')
+			this.$router.push("/")
 		}
 	}
 }
--- src/store/user.js
+++ src/store/user.js
@@ -39,11 +39,12 @@ export default {
 				});
 			}
 		},
+
 		async loginUser({ commit }, { email, password }) {
 			commit("clearError");
 			commit("setLoading", true);
 			//Здесь выполняется запрос на сервер
-			let isRequestOk = false;
+			let isRequestOk = true;
 			let promise = new Promise(function (resolve) {
 				setTimeout(() => resolve("Done"), 3000);
 			});
@@ -51,16 +52,21 @@ export default {
 			if (isRequestOk) {
 				await promise.then(() => {
 					commit("setUser", new User(1, email, password));
-					commit("setLoading", false);
+					commit("setLoading", true);
 				});
 			} else {
 				await promise.then(() => {
-					commit("setLoading", false);
+					commit("setLoading", true);
 					commit("setError", "Ошибка логина или пароля");
 					throw "Упс... Ошибка логина или пароля";
 				});
 			}
 		},
+
+		logoutUser({ commit }) {
+			commit('setUser', null);
+			commit('setLoading', false);
+		}
 	},
 	getters: {
 		user(state) {
