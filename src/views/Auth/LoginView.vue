7d9954830fa72899acf1c161ea53da95c277be9f
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 03:58:36 +0300

    14.2.  Отправка «loginUser» action с экрана логина

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -3,21 +3,24 @@
 		<v-row justify="center">
 			<v-col cols="12" sm="8" lg="6">
 				<v-card class="elevetion-12">
-					<v-toolbar dark color="primary" class="pl-4">
-						Login
+					<v-toolbar dark color="primary">
+						<v-toolbar-title>Login
+						</v-toolbar-title>
 					</v-toolbar>
 					<v-card-text>
 						<v-form v-model="valid" ref="form" validation>
-							<v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email" :rules="emailRules">
+							<v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email"
+								:rules="emailRules">
 							</v-text-field>
-							<v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password" :rules="passwordRules">
-							</v-text-field>
-
+							<v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password" v-model="password"
+								:rules="passwordRules"></v-text-field>
 						</v-form>
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer></v-spacer>
-						<v-btn color="primary" @click="onSubmit" :disabled="!valid">Login</v-btn>
+						<v-btn color="primary" @click="onSubmit" :disabled="!valid">
+							Login
+						</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-col>
@@ -37,21 +40,27 @@ export default {
 				v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
 			],
 			passwordRules: [
-				v => !!v || 'Password is required',
-				v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+				v => !!v || 'Name is required',
+				v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
 			]
 		}
 	},
 	methods: {
-		onSubmit(){
-			if (this.$refs.form.validate()){
+		onSubmit() {
+			if (this.$refs.form.validate()) {
 				const user = {
 					email: this.email,
 					password: this.password
 				}
-				console.log(user)
+				this.$store.dispatch('loginUser', user)
+					.then(() => {
+						this.$router.push("/")
+					})
+					.catch((err) => {
+						console.log(err)
+					})
 			}
 		}
 	}
-}
+} 
 </script>
